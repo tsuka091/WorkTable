8d6d145ac97fc3f2699b8b833ac7d4d62ee92e05
Tsukamoto <tsuka091@gmail.com>
2020-08-08 23:00:52 +0900

    下の方のセルに時間入るようにした

--- cell_createWorkTable.py
+++ cell_createWorkTable.py
@@ -3,8 +3,7 @@
 
 #Excelの操作に
 import openpyxl
-#ファイルのコピーに
-import shutil
+
 
 #ファイルを開く
 # data_only=True 関数のセルも値を読んでくる
@@ -14,39 +13,18 @@ sheet = book['1']
 
 #セル列定義
 # この辺はjsonファイルに定義して使う感じがいいのかな
-day_cell_num = 1
-start_cell_num = 3
-end_cell_num = 4
+day_column = 2
+start_column = 4
+end_column = 5
 
-for row in ws.iter_rows(min_row=5, max_row=35):
-    day = row[day_cell_num].value
-    if day == '土' or day == '日':
-        row[start_cell_num].value = ''
-        row[end_cell_num].value = ''
+for i in range(5, 66):
+    day = sheet.cell(row=i, column=day_column).value
+    if day == '土' or day == '日' or day == None:
+        continue
     else:
-        row[start_cell_num].value = '9:00'
-        row[end_cell_num].value = '17:30'        
+        sheet.cell(i + 1, start_column, '9:00')
+        sheet.cell(i + 1, end_column, '17:30')
 
 
 #保存
-book.save('workTable.xlsx')
-
-
-
-# #ワークブック新規作成
-# book = openpyxl.Workbook()
-
-# #シート名変更
-# sheet = book.active
-# sheet.title = 'sample'
-
-# #範囲を指定してセルを取得する
-# cells = sheet['A1':'B3']
-# i = 0
-# for row in cells:
-#     for cell in row:
-#         cell.value = i
-#         i += 1
-
-# #ワークブックに名前を付けて保存
-# book.save('sample.xlsx')
+book.save('workTable_cell.xlsx')
